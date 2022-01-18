891667a016113add472120abc90368e098e8f66d
Aksi <aksaule124026@gmail.com>
2022-01-18 20:16:07 +0600

    Learn request body and pydantic

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from schemas import Book
 
 
 app = FastAPI()
@@ -9,6 +10,16 @@ def home():
     return {'key': 'Hello'}
 
 
-@app.get('/{pk}')
+@app.get('/{pk}')#/5/?q="ttt"
 def get_item(pk: int, q: str = None):
     return {"key": pk, "q": q}
+
+
+@app.get('/user/{pk}/items/{item}/')
+def get_user_item(pk: int, item: str):
+    return {"user": pk, "item": item}
+
+
+@app.post('/book')
+def create_book(item: Book):
+    return item
