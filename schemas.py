c8e004116cb097aa592f5f0ee4348556dcaf2cfe
Aksi <aksaule124026@gmail.com>
2022-01-19 15:21:22 +0600

    Learn request body and pydantic1

---
+++ schemas.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+from datetime import date
+from typing import List
+
+
+class Genre(BaseModel):
+    name: str
+
+
+class Book(BaseModel):
+    title: str
+    writer: str
+    duration: str
+    date: date
+    summary: str
+    genres: List[Genre]
+    pages: int
